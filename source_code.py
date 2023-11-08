a66947567816737c64a2db0cc94bcfe130ae3616
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2023-11-08 13:56:01 +0200

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -3,36 +3,31 @@ import openpyxl
 from openpyxl.styles import PatternFill
 import io
 
-# Function to compare two Excel files
-def compare_excel_files(original_file, edited_file):
-    fill_style = PatternFill(start_color="FDD835", end_color="FDD835", fill_type="solid")
+# Open the original and edited workbooks
+original_file = openpyxl.load_workbook("original.xlsx")
+edited_file = openpyxl.load_workbook("edited.xlsx")
 
-    # Load the original and edited workbooks
-    original_data = openpyxl.load_workbook(original_file)
-    edited_data = openpyxl.load_workbook(edited_file)
+fill_style = PatternFill(start_color="FDD835", end_color="FDD835", fill_type="solid")
 
-    # Get a list of sheet names from the original workbook
-    sheet_names = original_data.sheetnames
+# Get a list of sheet names to compare (assuming the sheet names are the same in both workbooks)
+sheet_names = original_file.sheetnames
 
+# Iterate through the sheet names
+for sheet_name in sheet_names:
+    original_sheet = original_file[sheet_name]
+    edited_sheet = edited_file[sheet_name]
 
-    for sheet_name in sheet_names:
-        original_sheet = original_data[sheet_name]
-        edited_sheet = edited_data[sheet_name]
+    for row_original, row_edited in zip(original_sheet.iter_rows(), edited_sheet.iter_rows()):
+        for cell_original, cell_edited in zip(row_original, row_edited):
+            original_value = cell_original.value
+            edited_value = cell_edited.value
 
+            if original_value != edited_value:
+                cell_edited.fill = fill_style
 
-        for row_original, row_edited in zip(original_sheet.iter_rows(), edited_sheet.iter_rows()):
-            for cell_original, cell_edited in zip(row_original, row_edited):
-                original_value = cell_original.value
-                edited_value = cell_edited.value
-                
-                if original_value != edited_value:
-                    cell_edited.fill = fill_style
-
-    # Save the compared workbook and return the file bytes
-    
-    comapred_file = edited_file.save("compared_file.xlsx")
-    return comapred_file
-
+# Save the edited workbook
+edited_file.save("compared_file.xlsx")
+comapred_file = edited_file.save("compared_file.xlsx")
 # Streamlit app
 st.title("Excel File Comparison App")
 
