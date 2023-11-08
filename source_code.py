1b12b97c42a8d9fb9288a41e19ee3f8666cd771e
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2023-11-08 14:07:17 +0200

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -47,6 +47,6 @@ if original_file and edited_file:
     compared_filename = compare_excel_files(original_file, edited_file)
 
     st.success(f"Comparison complete. You can download the compared file from the link below:")
-    st.download_button("Download Compared File", data = compared_data)
+    st.download_button("Download Compared File", data = compared_filename)
 
 st.write("Note: This app assumes that the sheet names are the same in both files for comparison.")
