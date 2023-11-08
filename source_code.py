a4244d80ec160132c305a5041b3fdb4a606f2424
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2023-11-08 14:16:11 +0200

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -13,21 +13,30 @@ def compare_excel_files(original_file, edited_file):
     # Get a list of sheet names from the original workbook
     sheet_names = original_data.sheetnames
 
+    # Create a new workbook to store the compared data
+    compared_data = openpyxl.Workbook()
 
-    # Iterate through the sheet names
     for sheet_name in sheet_names:
-        original_sheet = original_file[sheet_name]
-        edited_sheet = edited_file[sheet_name]
-        for row_original, row_edited in zip(original_sheet.iter_rows(), edited_sheet.iter_rows()):
-            for cell_original, cell_edited in zip(row_original, row_edited):
+        original_sheet = original_data[sheet_name]
+        edited_sheet = edited_data[sheet_name]
+        compared_sheet = compared_data.create_sheet(sheet_name)
+
+        for row_original, row_edited, row_compared in zip(original_sheet.iter_rows(), edited_sheet.iter_rows(), compared_sheet.iter_rows()):
+            for cell_original, cell_edited, cell_compared in zip(row_original, row_edited, row_compared):
                 original_value = cell_original.value
                 edited_value = cell_edited.value
 
                 if original_value != edited_value:
-                    cell_edited.fill = fill_style
-    # Save the edited workbook
-    compared_file = edited_file.save("compared_file.xlsx")
-    return compared_file
+                    cell_compared.value = edited_value
+                    cell_compared.fill = fill_style
+                else:
+                    cell_compared.value = original_value
+
+    # Save the compared workbook and return the filename
+    compared_filename = "compared_file.xlsx"
+    compared_data.save(compared_filename)
+    return compared_filename
+
 # Streamlit app
 st.title("Excel File Comparison App")
 
@@ -38,6 +47,6 @@ if original_file and edited_file:
     compared_filename = compare_excel_files(original_file, edited_file)
 
     st.success(f"Comparison complete. You can download the compared file from the link below:")
-    st.download_button("Download Compared File", data = compared_file)
+    st.download_button("Download Compared File", compared_filename)
 
 st.write("Note: This app assumes that the sheet names are the same in both files for comparison.")
