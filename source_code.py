7ba8dd02be821df243cb5385af4c0721ac3e3a91
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2023-11-16 12:33:57 +0200

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import openpyxl
 from openpyxl.styles import PatternFill
-import os
+import io
 
 # Function to compare two Excel files
-@st.cache(allow_output_mutation=True)
-def load_excel(file):
-    return openpyxl.load_workbook(file)
-
-def compare_excel_files(original_data, edited_data):
+def compare_excel_files(original_file, edited_file):
     fill_style = PatternFill(start_color="FDD835", end_color="FDD835", fill_type="solid")
+
+    # Load the original and edited workbooks
+    original_data = openpyxl.load_workbook(original_file)
+    edited_data = openpyxl.load_workbook(edited_file)
 
     # Get a list of sheet names from the original workbook
     sheet_names = original_data.sheetnames
@@ -33,31 +33,20 @@ def compare_excel_files(original_data, edited_data):
                 else:
                     cell_compared.value = original_value
 
-    return compared_data
+    # Save the compared workbook and return the file bytes
+    compared_bytes = io.BytesIO()
+    compared_data.save(compared_bytes)
+    return compared_bytes
 
 # Streamlit app
 st.title("Excel File Comparison App")
 
-with st.expander("Upload Files"):
-    original_file = st.file_uploader("Upload the Original Excel File", type=["xlsx"])
-    edited_file = st.file_uploader("Upload the Edited Excel File", type=["xlsx"])
+original_file = st.file_uploader("Upload the Original Excel File", type=["xlsx"])
+edited_file = st.file_uploader("Upload the Edited Excel File", type=["xlsx"])
 
 if original_file and edited_file:
-    original_data = load_excel(original_file)
-    edited_data = load_excel(edited_file)
+    compared_bytes = compare_excel_files(original_file, edited_file)
 
-    compared_data = compare_excel_files(original_data, edited_data)
+    st.success("Comparison complete. You can download the compared file below:")
+    st.download_button("Download Compared File", data=compared_bytes, key="compared_file.xlsx")
 
-    # Save the compared workbook and return the filename
-    compared_filename = "compared_file.xlsx"
-    compared_data.save(compared_filename)
-
-    # Provide a download link for the compared file
-    st.success(f"Comparison complete. You can download the compared file from the link below:")
-    with open(compared_filename, "rb") as f:
-        st.download_button("Download Compared File", f.read(), key="download_button", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-
-# Cleanup the file after downloading
-os.remove(compared_filename)
-
-st.write("Note: This app assumes that the sheet names are the same in both files for comparison.")
