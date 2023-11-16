41df555d43f0803c461627b19c41b40fc488c344
Ali Maatouk <98925180+alimaatouq@users.noreply.github.com>
2023-11-16 13:45:07 +0200

    Update source_code.py

--- source_code.py
+++ source_code.py
@@ -4,7 +4,6 @@ from openpyxl.styles import PatternFill
 from io import BytesIO
 
 
-st.write('<style>div.block-container{padding-top:0.5rem;}</style>', unsafe_allow_html=True)
 
 def compare_and_save(original_file_path, edited_file_path):
     # Open the original and edited workbooks
